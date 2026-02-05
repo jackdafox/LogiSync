9325524cd624ad6708848daff296af25fc0e7aee
jackdafox <83963164+jackdafox@users.noreply.github.com>
2026-02-05 11:56:12 +0800

    adding modules

---
+++ example/examples.go
@@ -0,0 +1,27 @@
+package hello
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func HelloWorld(){
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+	})
+
+	http.ListenAndServe(":80", nil)
+
+	return
+}
+
+func DynamicRequest() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Welcome to my website!")
+	})
+
+	fs := http.FileServer(http.Dir("static/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.ListenAndServe(":80", nil)
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module logisync
+
+go 1.25.5
+
+require github.com/gorilla/mux v1.8.1 // indirect
+
+replace example/hello => ./example
+
+require example/hello v0.0.0
